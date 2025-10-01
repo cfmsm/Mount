6b498bdf8e96d725a48b6df4b4ef755996c09673
cfmsm <quellwrapsource@gmail.com>
2025-10-01 19:51:24 +0800

    Update Main.go

--- Main.go
+++ Main.go
@@ -1,23 +1,22 @@
 package main
+
 import (
     "fmt"
     "log"
     "net/http"
     "os"
 )
+
 func main() {
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fs := http.FileServer(http.Dir(cwd))
+    fs := http.FileServer(http.Dir("./files"))
     http.Handle("/", fs)
+
+    // Use Railway-assigned PORT
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-    fmt.Printf("Server starting on port %s, serving files from %s...\n", port, cwd)
-    if err := http.ListenAndServe(":"+port, nil); err != nil {
-        log.Fatal(err)
-    }
+
+    fmt.Printf("Server starting on port %s, serving files from ./files...\n", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
